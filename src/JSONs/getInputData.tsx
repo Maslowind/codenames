479df30721c24d7586c9106d0bedd3c092ab7932
maslowind <bart1885@gmail.com>
2020-04-28 15:46:19 +0300

    added json reader

---
+++ src/JSONs/getInputData.tsx
@@ -0,0 +1,9 @@
+
+let json_word = require("./input_data.json")
+export function getWords() {
+  return json_word.words;
+};
+export function getNumbers() {
+  return json_word.numbers;
+};
+
--- src/components/ColorField/ColorField.tsx
+++ src/components/ColorField/ColorField.tsx
@@ -1,12 +1,8 @@
 import * as React from 'react'
 import './ColorField.css';
-//import _ from "lodash"
-let data = [-1,
-  0, 0, 0, 0, 0, 0, 0,
-  1, 1, 1, 1, 1, 1, 1, 1,
-  2, 2, 2, 2, 2, 2, 2, 2, 2];
+import {getWords,getNumbers} from '../../JSONs/getInputData'
 
-  function resortArray(data:number[])
+  function resortArray(data:any[])
   {
 
       for (let i = data.length - 1; i >= 1; i--)
@@ -24,11 +20,16 @@ function printBtn() {
   const oldGrid= document.getElementById('GridCon');
   if (oldGrid) oldGrid.remove(); 
   const myDiv = document.createElement("div");
+  const word_massive = getWords();
+  let data = getNumbers();
+  resortArray(word_massive)
+  console.log(word_massive);
+  //console.log(number_massive[0]);
+  resortArray(data)
   myDiv.id='GridCon'
-  resortArray(data);
   for (let i = 0; i < 25; i++) {
     let btn = document.createElement("button");
-    //let t = document.createTextNode(`${i + 1}`);
+    let t = document.createTextNode(`${i + 1}`);
     if (data[i] === -1)
       btn.className = "Darkgreybutton";
     else if (data[i] === 0)
@@ -37,7 +38,7 @@ function printBtn() {
       btn.className = "Redbutton";
     else
       btn.className = "Bluebutton";
-    //btn.appendChild(t);
+    btn.appendChild(t);
     myDiv.appendChild(btn);
   
   }
@@ -49,6 +50,7 @@ function ColorField() {
     <div id="ColorField">
         <button className="ShowBtn" onClick={printBtn}>Show field</button>
     </div>
+    
   );
 }
 
